216bc6324d27f45b2db58c3b4019ee60002ad73b
ggb <ggb@informatik.uni-kiel.de>
2015-05-28 08:55:43 +0200

    add closeness centrality

---
+++ lib/forkingpaths/closeness_rank.ex
@@ -0,0 +1,29 @@
+defmodule ForkingPaths.ClosenessRank do
+  alias ForkingPaths.Helper
+
+  def closeness_rank(graph) do
+    vertices = :digraph.vertices(graph)
+    
+    vertices
+    |> Enum.map(fn vertex ->
+      val = Enum.reduce(vertices, 0, fn ele, acc ->
+        case :digraph.get_short_path(graph, vertex, ele) do
+          false         -> acc
+          vertices_list -> acc + length(vertices_list)
+        end
+      end)
+      
+      { vertex, 1 / val }
+    end)
+  end
+
+  def get(graph) do
+    graph
+    |> closeness_rank
+    |> Helper.normalize
+    |> Enum.sort(fn { _, fst }, { _, scd } -> 
+      fst > scd
+    end)
+  end
+
+end
